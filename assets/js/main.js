7d92ef1b5cdef14a9abbb4ebd65edf2c2ae8a1ec
Jaytech <90402385+Techguy012@users.noreply.github.com>
2023-11-03 00:12:14 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+/*=============== EMAIL VALIDATION ===============*/
+const form = document.getElementById('form'),
+      email = document.getElementById('email'),
+      pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+
+
+email.addEventListener('input', ()=> {
+
+    // Evaluate if the  matches the pattern values
+    if (email.value.match(pattern)) {
+        form.classList.add('valid')
+        form.classList.remove('invalid')
+    } else {
+        form.classList.add('invalid')
+        form.classList.remove('valid')
+    }
+
+    // If the input is empty, delete classes
+    if (email.value ==''){
+        form.classList.remove('invalid')
+        form.classList.remove('valid')
+    }
+})
